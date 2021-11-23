379035ac71f44f35868b47a7da4ad8d9c03f99e9
Roman Likhomanov <romanlee87@gmail.com>
2021-11-23 13:23:53 +0300

    installing CI, outline of the Api

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["jest", "@typescript-eslint"],
+  rules: {
+    "no-undef": "off",
+    "no-console": "off",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "no-plusplus": "off",
+    "default-case": "off",
+    "no-useless-escape": "off",
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+};
